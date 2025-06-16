b850df2198bf68141246b5311746cadcba366f53
mohammaddaud88 <daudansarikld@gmail.com>
2025-06-16 14:41:06 +0530

    home page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './Home'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        
+        <Route path='/' element={<Home/>}/>
       </Routes>
     </BrowserRouter>
   );
