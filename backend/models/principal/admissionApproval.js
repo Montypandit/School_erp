0ff46fbb925bc7cc91cbdc143b87e36dd845968d
mohammaddaud88 <daudansarikld@gmail.com>
2025-06-17 16:46:57 +0530

    document schema is created

--- backend/models/principal/admissionApproval.js
+++ backend/models/principal/admissionApproval.js
@@ -4,7 +4,7 @@ const admissionApprovalSchema = new mongoose.Schema({
     admissionId:{type:String, required:true, unique:true},
     inquiryId:{type:String, required:true, unique:true},
     admissionApproved:{type:String, default:'Pending'}
-});
+},{timestamps:true});
 
 const AdmissionApproval = mongoose.model('AdmissionApproval', admissionApprovalSchema);
 
