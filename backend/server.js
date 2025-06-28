db374921b2cc040bca92bdb98ad9e7b738fb032a
divyansh <divyanshkld899@gmail.com>
2025-06-28 12:09:31 +0530

    new changes

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,8 @@ const admissionFees = require('./controller/coordinator/admissionFeesApi')
 
 const examScheduleApi = require('./controller/admin/examScheduleapi');
 const leaveApi = require('./controller/admin/leaveApi');
+const activityPlannerRoutes = require('./controller/coordinator/Activityplannerapi');
+const monthlyPlannerRoutes = require('./controller/coordinator/Monthlyplannerapi');
 // Use APIs (with consistent route prefix)
 server.use('/api/auth', userLoginApi);
 server.use('/api/inquiry', inquiryApi);
@@ -43,6 +45,8 @@ server.use('/api/students', studentRoutes);
 server.use('/api/admission/fees',admissionFees);
 server.use('/api/exams', examScheduleApi);
 server.use('/api/leaves', leaveApi);
+server.use('/api/activity-planner', activityPlannerRoutes);
+server.use('/api/monthly/planner', monthlyPlannerRoutes);
 
 
 // Start server
